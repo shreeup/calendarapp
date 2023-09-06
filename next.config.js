962989973cf1e76b5beef59ba04de7088ef879a9
shreeup <shree912@yahoo.com>
2023-09-05 21:33:38 -0700

    add gitignore , env file

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    JWT_SECRET_KEY: "secret",
+    JWT_EXPIRES_IN: "1d",
+    MONGODB_URI:
+      "mongodb+srv://user:user@cluster0.0ql9mmu.mongodb.net/?retryWrites=true&w=majority",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
